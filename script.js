ff70803c3b5e0e10a8ba687422f41159203dbc6b
Jake Karl <jakekarl3@gmail.com>
2021-02-21 21:35:25 -0600

    Initial commit. Forgot to commit to repo

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+
+    // When guess is correct
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+  }
+});
