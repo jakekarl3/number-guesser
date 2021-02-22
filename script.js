2daae42f20073322289b993b4aafddc30d0dbc8f
Jake Karl <jakekarl3@gmail.com>
2021-02-21 21:48:06 -0600

    Complete play again function

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20);
+let secretNumber = Math.trunc(Math.random() * 20);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,6 +13,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // When guess is correct
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -42,4 +42,17 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game!';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  console.log('play again');
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20);
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
